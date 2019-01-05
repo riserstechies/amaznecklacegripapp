a975b0f7d28edca970f2484cfa48f4858a7ff298
riserstechies <46120655+riserstechies@users.noreply.github.com>
2019-01-05 20:47:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1; url=https://www.youtube.com/user/IanH/videos">   
+        <meta http-equiv="refresh" content="1; url=https://www.amazon.com/AUWU-Natural-Crystal-Necklace-Multicolor/dp/B07G42LLJV?pd_rd_w=FJMDG&pf_rd_p=c416ef13-a18e-4651-a317-e08f5b4cccea&pf_rd_r=1ab482d0-b75a-4065-9e7f-560701f711b9&pd_rd_r=1ab482d0-b75a-4065-9e7f-560701f711b9&pd_rd_wg=YV7J9&ref_=pd_gw_unk">   
         <script type="text/javascript">
-               window.location.href = "https://www.youtube.com/user/IanH/videos"
+               window.location.href = "https://www.amazon.com/AUWU-Natural-Crystal-Necklace-Multicolor/dp/B07G42LLJV?pd_rd_w=FJMDG&pf_rd_p=c416ef13-a18e-4651-a317-e08f5b4cccea&pf_rd_r=1ab482d0-b75a-4065-9e7f-560701f711b9&pd_rd_r=1ab482d0-b75a-4065-9e7f-560701f711b9&pd_rd_wg=YV7J9&ref_=pd_gw_unk"
         </script>
         <title>Page Redirection</title>
     </head>
